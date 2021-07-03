d68b26af1302779762b9e542584f65e0c96b0d3b
williamlatt <37807312+williamlatt@users.noreply.github.com>
2021-07-03 23:55:37 +0200

    push

--- index.js
+++ index.js
@@ -18,10 +18,61 @@ const io = require("socket.io")(server, {});
 let observerId = null;
 let operatorId = null;
 io.on('connection',socket=>{
+
   io.to(socket.id).emit('users',{
     'observerId':observerId,
     'operatorId':operatorId
   })
+
+  socket.on('joinOperator',data=>{
+    if (!operatorId) operatorId = socket.id;
+    io.emit('users',{
+      'observerId':observerId,
+      'operatorId':operatorId
+    })
+    io.to(operatorId).emit('operatorJoined',{});
+
+    if(operatorId && observerId){
+      io.to(operatorId).emit('ready',{});
+    }
+  })
+
+  socket.on('operator-offer', data => {
+    io.to(observerId).emit('operator-offer',data);
+  })
+  socket.on('observer-answer', data => {
+    io.to(operatorId).emit('observer-answer',data);
+  })
+
+  socket.on('joinObserver',data=>{
+    if (!observerId) observerId = socket.id;
+    io.emit('users',{
+      'observerId':observerId,
+      'operatorId':operatorId
+    })
+    io.to(observerId).emit('observerJoined',{});
+    if(operatorId && observerId){
+      io.to(operatorId).emit('ready',{});
+    }
+  })
+
+  socket.on('start-video', _ =>{
+    io.to(observerId).emit('start-stream');
+  })
+
+  socket.on('candidate-incoming', candidate => {
+    if(observerId === socket.id){
+      io.to(operatorId).emit('candidate', candidate);
+      console.log('candidate from observer')
+    }
+    if(operatorId === socket.id){
+      io.to(observerId).emit('candidate', candidate)
+      console.log('candidate from operator')
+    }
+  })
+
+
+  /*
   socket.on('joinOperator',data=>{
     if (!operatorId) operatorId = socket.id;
     io.emit('users',{
@@ -53,13 +104,13 @@ io.on('connection',socket=>{
     console.log(data)
     io.emit('iceCandidate',data);
   })
-
+*/
 
 
 
 })
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/mainapp.html');  
+  res.sendFile(__dirname + '/test.html');  
 });
 
 
